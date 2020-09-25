fc42c9dfe95f495e9e1209542384e8a2ebb4155d
kuceravyjura <kuceravyjura@gmail.com>
2020-09-25 22:35:18 +0300

    шестой проект

---
+++ Practice/06/C++/№6/№6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+
+    double a;
+    double c;
+    double b;
+    double x;
+    double x1;
+    cout << "введите переменную a"<<endl;
+    cin >> a;
+    cout << "введите переменную b" << endl; 
+    cin >> b;
+    cout << "введите переменную c" << endl; 
+    cin >> c;
+    
+     x = ((b * (-1)) + sqrt(b * b - (4 * a * c))) / (2 * a);
+     cout << x <<endl;
+     x1 = ((b * (-1)) - sqrt(b * b - (4 * a * c))) / (2 * a);
+     cout << x1 <<endl;
+}
