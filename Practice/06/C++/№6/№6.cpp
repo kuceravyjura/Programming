e2076705a3aa48733b32a98c0af85d67f2b0a30f
kuceravyjura <kuceravyjura@gmail.com>
2020-09-27 18:44:30 +0300

    practice6

--- Practice/06/C++/№6/№6.cpp
+++ Practice/06/C++/№6/№6.cpp
@@ -11,15 +11,66 @@ int main()
     double b;
     double x;
     double x1;
-    cout << "введите переменную a"<<endl;
+    cout << "введите переменную a" << endl;
     cin >> a;
-    cout << "введите переменную b" << endl; 
+    cout << "введите переменную b" << endl;
     cin >> b;
-    cout << "введите переменную c" << endl; 
+    cout << "введите переменную c" << endl;
     cin >> c;
-    
-     x = ((b * (-1)) + sqrt(b * b - (4 * a * c))) / (2 * a);
-     cout << x <<endl;
-     x1 = ((b * (-1)) - sqrt(b * b - (4 * a * c))) / (2 * a);
-     cout << x1 <<endl;
+    if (a != 0) {
+        if (b != 0) {
+            if (c != 0)
+            {
+                x = ((b * (-1)) + sqrt(b * b - (4 * a * c))) / (2 * a);
+                cout << "Первый корень" << x << endl;
+                x1 = ((b * (-1)) - sqrt(b * b - (4 * a * c))) / (2 * a);
+                cout << "второй корень" << x1 << endl;
+            }
+            else
+            {
+                x = 0;
+                cout << "Первый корень" << x << endl;
+                x1 = -b / a;
+                cout << "второй корень" << x1 << endl;
+
+            }
+        }
+        else {
+            if (c != 0) {
+                x = sqrt(-c / a);
+                cout << "Первый корень" << x << endl;
+                x1 = -sqrt(-c / a);
+                cout << "второй корень" << x1 << endl;
+
+            }
+            else {
+                cout << "x=0" << endl;
+
+            }
+
+        }
+    }
+    else {
+        if (b != 0) {
+            if (c != 0) {
+                x = -c / b;
+                cout << "корень равен" << x << endl;
+               
+            }
+            else {
+                cout << "корень = 0" << endl;
+                
+            }
+        }
+        else {
+            cout << "Корней нет"<< endl;
+        }
+    }
+
+
+
+
+
+
+
 }
