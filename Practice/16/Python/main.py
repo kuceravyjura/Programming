ffea1ec1867d78dec4a3448b530ae973941591a4
kuceravyjura <kuceravyjura@gmail.com>
2020-10-19 18:39:42 +0300

    16PY

--- Practice/16/Python/main.py
+++ Practice/16/Python/main.py
@@ -2,9 +2,10 @@ import re
 
 n = int(input())
 o = str()
-for i in range(0, n):
-    a = str(input())
-    b = re.fullmatch(r'a\w\d\d55661', a)
+num = input().split()
+for a in num:
+    for i in range(0, n):
+        b = re.fullmatch(r'a\w\d\d55661', a)
     if b:
         o = o + a
         o = o + " "
