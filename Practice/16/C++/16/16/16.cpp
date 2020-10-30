1fd2f28d5549796c5078057daac86200d57ddbda
kuceravyjura <kuceravyjura@gmail.com>
2020-10-30 19:42:43 +0300

    16

---
+++ Practice/16/C++/16/16/16.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string_view>
+using namespace std;
+
+int main() { 
+    int check = 0;
+    int count;string Tickets;string_view ticket;
+    cin >> count;
+   
+    for (int i = 0; i < count; ++i) {
+        cin >> Tickets;
+        ticket = Tickets;
+        if (ticket.starts_with('a') && ticket.ends_with("55661")) {
+            check = 1;
+            cout << ticket << " ";
+        }
+    }
+    if (check == 0) {
+        cout << "-1\n";
+    }
+}
