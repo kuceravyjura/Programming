870148c52f07682055aebe7fc31d57d8f217cfc8
kuceravyjura <kuceravyjura@gmail.com>
2020-11-13 11:52:33 +0300

    header1

---
+++ Practice/23/C++/23/23.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "Header.h"
+int main()
+{
+    int n;
+    std::cin >> n; 
+    std::cout << fact(n);
+}
---
+++ Practice/23/C++/23/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+int fact(int n) {
+    int x, i, count;
+    x = 1;
+    count = 1;
+    for (i = 0; i < n; i++) {
+        x = x * count;
+        count++;
+
+    }
+    n = x;
+    return n;
+}
