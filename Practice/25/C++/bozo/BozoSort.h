bdd63b77f84255620bdbc3b33f6276946742e960
kuceravyjura <kuceravyjura@gmail.com>
2020-12-25 21:16:18 +0300

    com

---
+++ Practice/25/C++/bozo/BozoSort.h
@@ -0,0 +1,129 @@
+#pragma once
+#include <iostream> 
+#include <ctime> 
+#include <vector> 
+#include <cmath> 
+using namespace std;
+template<class T>
+
+vector<T> BozoSort(vector<T>a, int n, bool y) {
+	int x1, x2, z = 0, check = 0;
+	vector <T> b(n);
+	while (1) {
+		x1 = rand() % n;
+		x2 = rand() % n;
+		if (x1 != x2) {
+			swap(a[x1], a[x2]);
+			for (int i = 0; i < n - 1; i++) {
+				if (a[i] <= a[i + 1]) {
+					z++;
+				}
+				else {
+					z = 0;
+					break;
+				}
+				if (z == n - 1) {
+					for (int i = 0; i < n; i++) {
+						b[i] = a[i];
+					}
+					check = 1;
+					break;
+				}
+			}
+			if (check == 1) {
+				if (y) {
+					for (int i = 0; i < n; i++) {
+						cout << b[i] << " ";
+					}
+				}
+				else if (y == 0) {
+					for (int i = n - 1; i >= 0; i--) {
+						cout << b[i] << " ";
+					}
+				}
+				cout << "\n";
+				return b;
+				break;
+			}
+		}
+	}
+}
+template<class T>
+vector<T> BozoSort(vector <vector < T>>a, int n, bool y) {
+	int y1, y2, y3, y4, chlen = 0, prov = 0, c = 0;
+	vector <T> b(n);
+	while (1)
+	{
+		int f = sqrt(n);
+		y1 = rand() % f;
+		y2 = rand() % f;
+		y3 = rand() % f;
+		y4 = rand() % f;
+		if (y1 != y3 || y2 != y4) {
+			swap(a[y1][y2], a[y3][y4]);
+			for (int i = 0; i <= sqrt(n) - 1; i++) {
+				for (int j = 0; j <= sqrt(n) - 1; j++) {
+					if (j == sqrt(n) - 1) {
+						if (i == sqrt(n) - 1) {
+							if (chlen == n - 1) {
+								for (int i = 0; i < sqrt(n); i++) {
+									for (int j = 0; j < sqrt(n); j++) {
+										b[c] = a[i][j];
+										c++;
+									}
+								}
+								prov = 1;
+								break;
+							}
+							else {
+								break;
+							}
+						}
+						if (a[i][j] <= a[i + 1][0]) {
+							chlen++;
+						}
+						else {
+							chlen = 0;
+							break;
+						}
+					}
+					else {
+						if (a[i][j] <= a[i][j + 1]) {
+							chlen++;
+						}
+						else {
+							chlen = 0;
+							break;
+						}
+					}
+				}
+			}
+
+			if (prov == 1) {
+				if (y) {
+					for (int i = 0; i < n; i++) {
+						cout << b[i] << " ";
+					}
+				}
+				else if (y == 0) {
+					for (int i = n - 1; i >= 0; i--) {
+						cout << b[i] << " ";
+					}
+				}
+				cout << "\n";
+				return b;
+				break;
+			}
+		}
+	}
+}
+
+template<class T>
+vector<T> BozoSort(vector<T>a, bool y) {
+	if (y == 1) {
+		return BozoSort(a, 3, y);
+	}
+	else
+		return BozoSort(a, 3, y);
+}
+
---
+++ Practice/29/C++/29/29.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include "BozoSort.h"
+
+using namespace std;
+
+struct student{
+    string name;
+    short group;
+    map<string, short> exams;
+    student(std::string name, int group,
+        int math, int phys, int hist, int prog);
+
+
+};
+
+
+
+int main()
+{
+    int n; cin >> n;
+    vector<string> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "+------------+-------+------+------+------+------+" << endl;
+    cout << "| Name       | Group | Math | Phys | Hist | Prog |";
+    cout << "+------------+-------+------+------+------+------+" << endl;
+    vector<student> students{
+        student("Lenin W",            3,4,3,5,5),
+        student("Kurilovich J",       5,2,2,3,5),
+        student("Bezdarev K",         3,4,4,4,4),
+        student("Lucifer S",          5,3,4,3,4),
+        student("Sigizmund V",        3,4,4,2,2),
+        student("Shepard S",          3,5,4,5,3),
+        student("Ainz O",             2,5,2,5,4),
+        student("Octavios T",         4,3,2,5,5),
+    };
+    //BozoSort(students,8 , 1); фигово как-то работает
+
+}
